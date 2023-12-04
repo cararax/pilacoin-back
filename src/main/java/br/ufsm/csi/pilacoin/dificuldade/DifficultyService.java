2936e21f1d1ba602829acb5f070c6cd6c8b01097
Pedro Carara <cararax@gmail.com>
2023-12-04 08:03:46 -0300

    refactor: better code readability

--- src/main/java/br/ufsm/csi/pilacoin/dificuldade/DifficultyService.java
+++ src/main/java/br/ufsm/csi/pilacoin/dificuldade/DifficultyService.java
@@ -4,60 +4,55 @@ import br.ufsm.csi.pilacoin.mock.dto.Dificuldade;
 import br.ufsm.csi.pilacoin.pila.PilaService;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import lombok.RequiredArgsConstructor;
+import lombok.SneakyThrows;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.time.Instant;
+import java.util.Objects;
 
 @Service
 @Log4j2
+@RequiredArgsConstructor
 public class DifficultyService {
 
-    @Autowired
     PilaService pilaService;
-
-    private ObjectMapper mapper = new ObjectMapper();
-
+    private final ObjectMapper mapper = new ObjectMapper();
     private Dificuldade dificuldadeAtual = new Dificuldade();
 
+    @SneakyThrows
     @RabbitListener(queues = "${queue.dificuldade}")
     public void listen(String message) {
-        try {
-            Dificuldade dificuldadeRecebida = convertJsonToDifficulty(message);
-            if (isDifficultyChanged(dificuldadeAtual, dificuldadeRecebida)) {
-                logDifficultyChange(dificuldadeAtual, dificuldadeRecebida);
-                dificuldadeAtual = dificuldadeRecebida;
-                if (isDifficultyValid(dificuldadeAtual)) {
-                    pilaService.startMining(dificuldadeAtual);
-                }
-            }
-        } catch (JsonProcessingException e) {
-            log.error("Erro ao interpretar mensagem de dificuldade: {}", e.getMessage());
-        }
+        Dificuldade dificuldadeRecebida = convertJsonToDifficulty(message);
+        if (isDifficultyUnchanged(dificuldadeAtual, dificuldadeRecebida)) return;
+        dificuldadeAtual = dificuldadeRecebida;
+        if (isDifficultyInvalid(dificuldadeAtual)) return;
+        pilaService.startMining(dificuldadeAtual);
     }
 
-    private boolean isDifficultyChanged(Dificuldade dificuldadeAtual, Dificuldade dificuldadeRecebida) {
-        return !dificuldadeAtual.equals(dificuldadeRecebida);
-    }
-
-    private void logDifficultyChange(Dificuldade dificuldadeAtual, Dificuldade dificuldadeRecebida) {
-        if (dificuldadeAtual != null)
-            log.info("Dificuldade atual antes da atualização: {}", dificuldadeAtual);
+    private boolean isDifficultyUpdated(Dificuldade dificuldadeAtual, Dificuldade dificuldadeRecebida) {
+        log.info((dificuldadeAtual != null) ? "Dificuldade anterior: {}" : "", dificuldadeAtual);
         log.info("Dificuldade atualizada. Valor: {}", dificuldadeRecebida);
+        return !Objects.equals(dificuldadeAtual, dificuldadeRecebida);
+    }
+
+    private boolean isDifficultyUnchanged(Dificuldade dificuldadeAtual, Dificuldade dificuldadeRecebida) {
+        return !isDifficultyUpdated(dificuldadeAtual, dificuldadeRecebida);
     }
 
     private boolean isDifficultyValid(Dificuldade dificuldade) {
-        long nowMillis = System.currentTimeMillis();
-        Instant now = Instant.ofEpochMilli(nowMillis);
+        Instant now = Instant.ofEpochMilli(System.currentTimeMillis());
         Instant validadeFinal = Instant.ofEpochMilli(dificuldade.getValidadeFinal());
-        if (now.isAfter(validadeFinal)) {
-            log.info("Dificuldade expirou. Hora atual: {}, Validade Final: {}", now, validadeFinal);
-            return false;
-        }
-        log.info("Dificuldade ainda é válida. Hora atual: {}, Validade Final: {}", now, validadeFinal);
-        return true;
+        boolean isValid = now.isBefore(validadeFinal);
+        String status = isValid ? "válida" : "inválida";
+        log.info("A dificuldade é {}. Hora atual: {}, Validade Final: {}", status, now, validadeFinal);
+        return isValid;
+    }
+
+    private boolean isDifficultyInvalid(Dificuldade dificuldade) {
+        return !isDifficultyValid(dificuldade);
     }
 
     private Dificuldade convertJsonToDifficulty(String message) throws JsonProcessingException {
--- src/main/java/br/ufsm/csi/pilacoin/pila/PilaService.java
+++ src/main/java/br/ufsm/csi/pilacoin/pila/PilaService.java
@@ -3,14 +3,22 @@ package br.ufsm.csi.pilacoin.pila;
 import br.ufsm.csi.pilacoin.key.KeyPairGenerator;
 import br.ufsm.csi.pilacoin.mock.dto.Dificuldade;
 import br.ufsm.csi.pilacoin.mock.dto.PilaCoin;
-import com.fasterxml.jackson.core.JsonProcessingException;
+import br.ufsm.csi.pilacoin.mock.dto.ValidacaoPilaCoin;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.RequiredArgsConstructor;
+import lombok.SneakyThrows;
 import lombok.extern.log4j.Log4j2;
+import org.springframework.amqp.rabbit.annotation.RabbitListener;
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
+import java.nio.charset.StandardCharsets;
+import java.security.MessageDigest;
+import java.security.Signature;
+import java.util.Collections;
+import java.util.HashSet;
+import java.util.Set;
 import java.util.concurrent.atomic.AtomicReference;
 
 @Service
@@ -26,26 +34,104 @@ public class PilaService {
 
     @Value("${queue.pila-minerado}")
     private String pilaMineradoQueue;
+    @Value("${queue.pila-validado}")
+    private String pilaValidadoQueue;
+
+    @Value("${pilacoin.username:carara-schmitzhaus}")
+    private String username;
+
+    private MinerWorker minerWorker;
+    private ObjectMapper mapper = new ObjectMapper();
+    private final Set<PilaCoin> minedPilaCoins = Collections.synchronizedSet(new HashSet<>());
 
     public void startMining(Dificuldade dificuldade) {
         this.dificuldadeAtual.set(dificuldade);
-        int numeroDeThreads = Runtime.getRuntime().availableProcessors();
+        int threadNumber = Runtime.getRuntime().availableProcessors();
         log.info("Começando a minerar com a nova dificuldade: {}", dificuldadeAtual);
-        log.info("Número de threads: {}", numeroDeThreads);
+        log.info("Número de threads: {}", threadNumber);
 
-        for (int i = 0; i < numeroDeThreads; i++) {
-            MinerWorker minerWorker = new MinerWorker("Thread-" + i, dificuldadeAtual, keyPairGenerator, this);
+        for (int i = 0; i < threadNumber; i++) {
+            minerWorker = new MinerWorker("Thread-" + i, dificuldadeAtual, keyPairGenerator, this);
             minerWorker.start();
         }
     }
 
+    @SneakyThrows
     public void publishMinedPila(PilaCoin pilaCoin) {
-        try {
-            rabbitTemplate.convertAndSend(pilaMineradoQueue, new ObjectMapper().writeValueAsString(pilaCoin));
-            log.info("PilaCoin sent to queue");
-        } catch (JsonProcessingException e) {
-            log.error("Error converting PilaCoin to JSON string: {}", e.getMessage());
+        rabbitTemplate.convertAndSend(pilaMineradoQueue, convertToJson(pilaCoin));
+        log.info("PilaCoin sent to mined queue");
+    }
+
+    @SneakyThrows
+    public void publishValidatedPila(ValidacaoPilaCoin validatedPilaCoin) {
+        rabbitTemplate.convertAndSend(pilaValidadoQueue, convertValidatedToJson(validatedPilaCoin));
+        log.info("PilaCoin sent to validated queue");
+    }
+
+    @SneakyThrows
+    @RabbitListener(queues = "${queue.pila-minerado}")
+    public void validatePilaCoin(String message) {
+        PilaCoin pilaCoin = mapper.readValue(message, PilaCoin.class);
+        if (isCreatedByCurrentUser(pilaCoin) || isAlreadyValidated(pilaCoin) || isPilaInvalid(pilaCoin)) {
+            publishMinedPila(pilaCoin);
+            return;
         }
+        minedPilaCoins.add(pilaCoin);
+        publishValidatedPila(generateValidationMessage(pilaCoin));
+        log.info("PilaCoin validated and sent to queue");
+    }
+
+
+    private boolean isPilaValid(PilaCoin pilaCoin) {
+        return minerWorker.hashMeetsDifficulty(pilaCoin, dificuldadeAtual.get());
+    }
+
+    private boolean isPilaInvalid(PilaCoin pilaCoin) {
+        return !isPilaValid(pilaCoin);
+    }
+
+    private boolean isCreatedByCurrentUser(PilaCoin pilaCoin) {
+        return pilaCoin.getNomeCriador().equals(username);
+    }
+
+    @SneakyThrows
+    private void resendToQueue(PilaCoin pilaCoin) {
+        rabbitTemplate.convertAndSend(pilaMineradoQueue, convertPilaToJson(pilaCoin));
+        log.info("PilaCoin resent to queue");
+    }
+
+    private boolean isAlreadyValidated(PilaCoin pilaCoin) {
+        return minedPilaCoins.contains(pilaCoin);
+    }
+
+    @SneakyThrows
+    private ValidacaoPilaCoin generateValidationMessage(PilaCoin pilaCoin) {
+        Signature signature = Signature.getInstance("SHA256withRSA");
+        signature.initSign(keyPairGenerator.getPrivateKey());
+
+        String pilaJson = mapper.writeValueAsString(pilaCoin);
+
+        MessageDigest sha256 = MessageDigest.getInstance("SHA-256");
+        byte[] pilaHash = sha256.digest(pilaJson.getBytes(StandardCharsets.UTF_8));
+        signature.update(pilaHash);
+
+        byte[] signatureBytes = signature.sign();
+        return ValidacaoPilaCoin.builder().nomeValidador(username).chavePublicaValidador(keyPairGenerator.getPublicKey().getEncoded()).assinaturaPilaCoin(signatureBytes).pilaCoinJson(pilaCoin).build();
+    }
+
+    @SneakyThrows
+    private String convertToJson(Object object) {
+        return mapper.writeValueAsString(object);
+    }
+    @SneakyThrows
+    private String convertPilaToJson(PilaCoin pila) {
+        return mapper.writeValueAsString(pila);
+    }
+
+    @SneakyThrows
+    private String convertValidatedToJson(ValidacaoPilaCoin validated) {
+        return mapper.writeValueAsString(validated);
     }
 
 }
+
