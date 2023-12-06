d9ba9c00536604ffee37b5d8ea38c142977ace11
Pedro Carara <cararax@gmail.com>
2023-12-06 15:59:45 -0300

    feat: add report service to print logs

---
+++ src/main/java/br/ufsm/csi/pilacoin/dificuldade/ReportService.java
@@ -0,0 +1,19 @@
+package br.ufsm.csi.pilacoin.dificuldade;
+
+import lombok.RequiredArgsConstructor;
+import lombok.SneakyThrows;
+import lombok.extern.log4j.Log4j2;
+import org.springframework.amqp.rabbit.annotation.RabbitListener;
+import org.springframework.stereotype.Service;
+
+@Service
+@Log4j2
+@RequiredArgsConstructor
+public class ReportService {
+
+    @SneakyThrows
+    @RabbitListener(queues = "${queue.report}")
+    public void listen(String message) {
+        log.info("REPORT: {}", message);
+    }
+}
